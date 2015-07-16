df60f626a88cd856d08fabc7858f7e84aab70c1a
Eduard <edwardintoronto@gmail.com>
2015-07-16 16:00:20 -0500

    Update header.php

--- header.php
+++ header.php
@@ -40,12 +40,6 @@
                             <div class="col-md-6">
                                 <div class="main-menu">
                                 <?php wp_nav_menu(); ?> 
-                                    <!--<ul>
-                                        <li><a href="index.html">Home</a></li>
-                                        <li><a href="about-us.html">About</a></li>
-                                        <li><a href="products.html">Products</a></li>
-                                        <li><a href="contact-us.html">Contact</a></li>
-                                    </ul> -->
                                 </div>
                             </div>
                             <div class="col-md-3">
